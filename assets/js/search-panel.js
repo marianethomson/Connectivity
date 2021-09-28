6065997b48e2902a725a981991d306246ed5114a
Mariane Thomson <marianedinis@gmail.com>
2021-09-28 19:23:36 +1000

    checking branch

--- assets/js/search-panel.js
+++ assets/js/search-panel.js
@@ -1,9 +1,9 @@
 function showSearchPanel() {
-    $('#search-panel').show();
-    $('#search-results-panel').hide();
-    $('#favourites-panel').hide();
+  $("#search-panel").show();
+  $("#search-results-panel").hide();
+  $("#favourites-panel").hide();
 }
 
 $(function () {
-    $('#search-btn').click(showSearchResultsPanel);
-   });
+  $("#search-btn").click(showSearchResultsPanel);
+});
