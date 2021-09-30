9e16772ca579166ad6e453d3022335f7daf8d461
Mariane Thomson <marianedinis@gmail.com>
2021-09-30 19:56:10 +1000

    nyt api call

--- assets/js/search-panel.js
+++ assets/js/search-panel.js
@@ -1,3 +1,5 @@
+var apiKey = "C1TDrksFmBX6rwRPyWGH6t6yIkYDeYxq";
+
 function showSearchPanel() {
   $("#search-panel").show();
   $("#search-results-panel").hide();
@@ -5,5 +7,44 @@ function showSearchPanel() {
 }
 
 $(function () {
-  $("#search-btn").click(showSearchResultsPanel);
+  $("#search-btn").on("click", showSearchResultsPanel, function (event) {
+    event.preventDefault();
+    getMovies();
+  });
+
+  // process form
 });
+
+//get search parameters
+function getSearchParam() {
+  var param = $("#search").val();
+  var infoStatus = $("#infoStatus");
+  if (param == 0) {
+    infoStatus.text = "Please inform something to search for.";
+  } else {
+    return param;
+  }
+}
+
+//calls the NYT Movies API
+function getMovies() {
+  var param = getSearchParam();
+  var apiUrl =
+    "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" +
+    param +
+    "&api-key=" +
+    apiKey;
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to server");
+    });
+}
