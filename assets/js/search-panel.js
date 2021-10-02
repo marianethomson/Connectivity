8aef382b027fb031714b252d653ea60e454237b9
Mariane Thomson <marianedinis@gmail.com>
2021-10-03 04:30:23 +1100

    refactoring api calls and adding movie display, needs image

--- assets/js/search-panel.js
+++ assets/js/search-panel.js
@@ -1,5 +1,7 @@
 var apiMoviesKey = "kxGjghc3HTcDjcm1Tq3Cp4Uj0TpCC4Dd";
+var apiBooksKey = "C1TDrksFmBX6rwRPyWGH6t6yIkYDeYxq";
 var baseMoviesURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+var baseBooksURL = "https://api.nytimes.com/svc/books/v3/reviews.json";
 
 function showSearchPanel() {
   $("#search-panel").show();
@@ -25,31 +27,36 @@ $(function () {
   $("#search-btn").on("click", showSearchResultsPanel, function (event) {
     event.preventDefault();
     //search only by movie
-    if (getCheckBoxMovies() == true) {
+    if (getCheckBoxMovies()) {
       getMoviesByParam();
     } //search only by book
-    else if (getCheckBoxBooks() == true) {
-      getUserChoicebyAuthor(authorName);
-      getUserChoiceByTitle(userChoicebyTitle);
+    else if (getCheckBoxBooks()) {
+      getUserChoicebyAuthor();
+      getUserChoiceByTitle();
     }
     //search all
     else {
       getMoviesByParam();
-      getUserChoicebyAuthor(authorName);
-      getUserChoiceByTitle(userChoicebyTitle);
+      getUserChoicebyAuthor();
+      getUserChoiceByTitle();
     }
   });
 
   // process form
 });
-
+//tests movies critics-picks
 $(function () {
   $("#critics-btn").on("click", showSearchResultsPanel, function (event) {
     event.preventDefault();
     getMoviesPicks();
   });
-
-  // tests critics call - ok
+});
+//tests books list best sellers
+$(function () {
+  $("#books-lists-btn").on("click", showSearchResultsPanel, function (event) {
+    event.preventDefault();
+    getTopSellers();
+  });
 });
 
 //get search parameters
@@ -65,47 +72,47 @@ function getSearchParam() {
 
 //calls the NYT Movies API and get the critics picks
 function getMoviesPicks() {
-  var criticsBtn = $("#critics-btn");
-  var apiUrl = baseMoviesURL + "?critics-pick=Y" + "&api-key=" + apiMoviesKey;
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
-        });
-      } else {
-        alert("Error: " + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert("Unable to connect to server");
-    });
+  //  var criticsBtn = $("#critics-btn");
+  var apiPicksUrl =
+    baseMoviesURL + "?critics-pick=Y" + "&api-key=" + apiMoviesKey;
+  getMovieDetails(apiPicksUrl);
 }
 
 //calls the NYT Movies API search movies by param
 function getMoviesByParam() {
   var param = getSearchParam();
-  var apiUrl = baseMoviesURL + "?query=" + param + "&api-key=" + apiMoviesKey;
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
-        });
-      } else {
-        alert("Error: " + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert("Unable to connect to server");
-    });
+  var apiQueryUrl =
+    baseMoviesURL + "?query=" + param + "&api-key=" + apiMoviesKey;
+  getMovieDetails(apiQueryUrl);
+}
+
+// Function to get book details by user's choice of title
+function getUserChoiceByTitle() {
+  var userChoicebyTitle = getSearchParam();
+  console.log(userChoicebyTitle + " step1");
+  var searchByTitleUrl = new URL(
+    baseBooksURL + "?title=" + userChoicebyTitle + "&api-key=" + apiBooksKey
+  );
+  getBookDetails(searchByTitleUrl);
+}
+
+// Function to get user choice  by author's name
+
+function getUserChoicebyAuthor() {
+  var authorName = getSearchParam();
+  var searchByAuthorUrl = new URL(
+    baseBooksURL + "?author=" + authorName + "&api-key=" + apiBooksKey
+  );
+
+  getBookDetails(searchByAuthorUrl);
 }
 
 // Function to get Books data
 
 function getTopSellers() {
   var topSellerBooksUrl = new URL(
-    "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=C1TDrksFmBX6rwRPyWGH6t6yIkYDeYxq"
+    "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=" +
+      apiBooksKey
   );
 
   console.log(topSellerBooksUrl);
@@ -124,32 +131,6 @@ function getTopSellers() {
     .catch(function (error) {
       console.error(error);
     });
-}
-
-// Function to get book details by user's choice of title
-
-function getUserChoiceByTitle(userChoicebyTitle) {
-  console.log(userChoicebyTitle +" step1");
-  var searchByTitleUrl = new URL(
-    "https://api.nytimes.com/svc/books/v3/reviews.json?title=" +
-      userChoicebyTitle +
-      "&api-key=C1TDrksFmBX6rwRPyWGH6t6yIkYDeYxq"
-  );
-
-  getBookDetails(searchByTitleUrl);
-}
-
-// Function to get user choice  by author's name
-
-function getUserChoicebyAuthor(authorName) {
-
-  var searchByAuthorUrl = new URL(
-    "https://api.nytimes.com/svc/books/v3/reviews.json?author=" +
-      authorName +
-      "&api-key=C1TDrksFmBX6rwRPyWGH6t6yIkYDeYxq"
-  );
-
-  getBookDetails(searchByAuthorUrl);
 }
 
 // Function to retrieve data for search by title/author
@@ -172,10 +153,63 @@ function getBookDetails(searchURL) {
     });
 }
 
+function getMovieDetails(searchURL) {
+  fetch(searchURL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (queryRes) {
+      if (queryRes.num_results > 0) {
+        renderMovieResult(queryRes);
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+function renderMovieResult(queryRes) {
+  $("#movieResults").html("");
+  var innerHTML = "";
+  queryRes.results.forEach((result) => {
+    innerHTML += renderMovieResultTemplate(result);
+  });
+  $("#movieResults").html(innerHTML);
+}
+
+// Function to display books by an author or title
+//fix image - works for critics picks, but doesn't for searched movie?!
+//<div class="media">
+//      <div class="media-left">
+//      <figure class="image is-48x48">
+//      <img onerror="this.src='./assets/images/no-image.jpg';this.onerror='';" src="${result.multimedia.src}" alt="${result.display_title}">
+//      </figure>
+//     </div>
+
+function renderMovieResultTemplate(result) {
+  return `
+  <div class="card">
+    <div class="card-content">
+        <div class="media-content">
+          <p class="title is-4">${result.display_title}</p>
+          <p class="subtitle is-6">Rating: ${result.mpaa_rating}</p>
+        </div>
+      </div>
+      <div class="content">
+      ${result.summary_short}
+        <br>
+        <time datetime>Release Date: ${result.opening_date}</time>
+      </div>
+    </div>
+  </div>`;
+}
+
 // Functionto display the top 5 bestsellers - books
 
 // Display results for the user's choice of book by title
-
 
 function renderBookResult(queryRes) {
   $("#bookResults").html("");
@@ -186,11 +220,10 @@ function renderBookResult(queryRes) {
   $("#bookResults").html(innerHTML);
 }
 
-
 // Function to display books by an author or title
 
-function renderBookResultTemplate(result){
-   return `
+function renderBookResultTemplate(result) {
+  return `
   <div class="card">
     <div class="card-content">
       <div class="media">
@@ -213,22 +246,20 @@ function renderBookResultTemplate(result){
   </div>`;
 }
 
-
 /*  getTopSellers();  */
 
 // Function to display top five books
 
-function renderTopSellers(queryRes){
+function renderTopSellers(queryRes) {
   $("#topBookResults").html("");
   var innerHTML = "";
- queryRes.slice(0,5).forEach(result => {
-   innerHTML += renderTopFiveBookResultTemplate(result);
- });
- $("#topBookResults").html(innerHTML);
+  queryRes.slice(0, 5).forEach((result) => {
+    innerHTML += renderTopFiveBookResultTemplate(result);
+  });
+  $("#topBookResults").html(innerHTML);
 }
 
-
-function renderTopFiveBookResultTemplate(result){
+function renderTopFiveBookResultTemplate(result) {
   return `<article class="media">
   <figure class="media-left">
     <p class="image is-64x64">
@@ -248,7 +279,8 @@ function renderTopFiveBookResultTemplate(result){
   <div class="media-right">
     <button class="is-danger">&#x2764;</button>
   </div>
-</article>`}
+</article>`;
+}
 
 //Monitors the checkboxes values
 $(document).ready(getCheckBoxMovies);
